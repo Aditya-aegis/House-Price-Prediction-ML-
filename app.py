c2fccc676eadf50b2d51e2c6aac3bb4686538f33
Aditya(Aegis) <228388453+Aditya-aegis@users.noreply.github.com>
2025-12-28 16:20:47 +0530

    Correct typos and enhance output formatting
    Fix typos and improve house price prediction display

--- app.py
+++ app.py
@@ -34,10 +34,11 @@ house_price = model.pridect(final_value)[0]
 
 with st.spinner('Predicting House price'):
   time.sleep(1)
-msg = f'''House price is: ${house_price*100000}'''
+msg = f'''House price is: ${round(house_price*100000,2)}'''
 st.success(msg)
 
 st.markdown('''**Deign and Developed by: Aditya Kumar**''')
+
 
 
 
