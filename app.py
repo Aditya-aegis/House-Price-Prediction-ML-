d9ee8071597d82e6be949c9cd54e94a8ffabe0bc
Aditya(Aegis) <228388453+Aditya-aegis@users.noreply.github.com>
2025-12-28 15:38:24 +0530

    Refactor house price prediction app for accuracy
    Fixed variable name typos and corrected slider function call.

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ st.sidebar.title('Selet House feature')
 st.sidebar.image('https://i.pinimg.com/236x/08/b1/84/08b184bf0fa126158ed3d56a42e20414.jpg')
 all_value = []
 for i in X:
-  ans = st.siderbar.slider(f'selet {i} value')
+  min_value = int(X[i].min())
+  max_vlaue = int(X[i].max())
+  ans = st.siderbar.slider(f'selet {i} value', min_value, max_value)
   all_vlaue.append(ans)  
 
-st.write(all_value)
+# st.write(all_value)
+
